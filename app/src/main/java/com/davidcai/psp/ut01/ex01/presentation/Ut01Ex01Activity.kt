d224a5b65aacbd694be7b8f19b927a371c18e54a
David <104197044+DavidCaidas@users.noreply.github.com>
2022-09-28 19:02:06 +0200

    psp2022

---
+++ app/src/main/java/com/davidcai/psp/ut01/ex01/presentation/Ut01Ex01Activity.kt
@@ -0,0 +1,31 @@
+package com.davidcai.psp.ut01.ex01.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.davidcai.psp.R
+
+class Ut01Ex01Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01ex01)
+         val label: TextView= findViewById(R.id.label)
+        label.text= getString(R.string.title)
+
+
+        Thread{
+            Thread.sleep(2000)
+            runOnUiThread {
+
+                label.text="cambiando"
+                }
+            Log.d("@dev","Thread1")
+
+
+
+        }.start()
+
+    }
+
+}
